704427208305f60bcb66b8d3c27cf14e51f42d0d
candyce <candyce005@gmail.com>
2026-03-26 19:13:55 +0800

    Added UsableItem.java
    Added name and constructor

--- projCharacterClasses/UsableItem.java
+++ projCharacterClasses/UsableItem.java
@@ -1,5 +1,11 @@
 package projCharacterClasses;
 
 public abstract class UsableItem {
+	private String name;
 
+	public UsableItem(String name) {
+		this.name = name;
+	}
+
+	public abstract void use(Combatant user, Combatant target);
 }
